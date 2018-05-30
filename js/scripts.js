d391674d6b036201a5d3ce4e90086a02c1df0fca
Austin Barr and Nick Rogers <nick@>
2018-05-30 09:50:53 -0700

    Add each word and their definition.

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,64 @@ $(document).ready(function(){
     $(".Variable-naming-conventionsWord").toggle();
     $(".Variable-naming-conventionsDefinition").toggle();
   });
+  $("#Functions").click(function() {
+    $(".FunctionsWord").toggle();
+    $(".FunctionsDefinition").toggle();
+  });
+  $("#Methods").click(function() {
+    $(".MethodsWord").toggle();
+    $(".MethodsDefinition").toggle();
+  });
+  $("#Arguments").click(function() {
+    $(".ArgumentsWord").toggle();
+    $(".ArgumentsDefinition").toggle();
+  });
+  $("#Parameters").click(function() {
+    $(".ParametersWord").toggle();
+    $(".ParametersDefinition").toggle();
+  });
+  $("#Return").click(function() {
+    $(".ReturnWord").toggle();
+    $(".ReturnDefinition").toggle();
+  });
+  $("#Chaining-methods").click(function() {
+    $(".Chaining-methodsWord").toggle();
+    $(".Chaining-methodsDefinition").toggle();
+  });
+  $("#Strings").click(function() {
+    $(".StringsWord").toggle();
+    $(".StringsDefinition").toggle();
+  });
+  $("#Boolean").click(function() {
+    $(".BooleanWord").toggle();
+    $(".BooleanDefinition").toggle();
+  });
+  $("#Undefined").click(function() {
+    $(".UndefinedWord").toggle();
+    $(".UndefinedDefinition").toggle();
+  });
+  $("#NaN").click(function() {
+    $(".NaNWord").toggle();
+    $(".NaNDefinition").toggle();
+  });
+  $("#Escape").click(function() {
+    $(".EscapeWord").toggle();
+    $(".EscapeDefinition").toggle();
+  });
+  $("#Alert").click(function() {
+    $(".AlertWord").toggle();
+    $(".AlertDefinition").toggle();
+  });
+  $("#Comments").click(function() {
+    $(".CommentsWord").toggle();
+    $(".CommentsDefinition").toggle();
+  });
+  $("#jQuery").click(function() {
+    $(".jQueryWord").toggle();
+    $(".jQueryDefinition").toggle();
+  });
+  $("#Attributes").click(function() {
+    $(".AttributesWord").toggle();
+    $(".AttributesDefinition").toggle();
+  });
 });
